d7a3d97850cc5d371d8f0a91f9ebf3674773b25b
Mokhtar Ali <Joemokhtarali@gmail.com>
2022-01-25 21:01:51 -0500

    changed navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import '../style/navbar.css' 
 // import Link from 'react-router-dom'
@@ -8,17 +9,17 @@ export default function NavBar() {
     return (
         <header className='toolbar'>
             <nav className='toolbar_navigation'> 
-            <div className='space-1'></div>
-                <div className='toolbar_navigation-items'>
-                    <ul >
-                        <li ><a className='navbar-li' href='/' > Home </a></li>
-                        <li ><a className='navbar-li' href='/about'> About </a></li>
+            {/* <div className='space-1'></div> */}
+                {/* <div > */}
+                    <ul className='toolbar_navigation-items' >
+                        <li ><a className='navbar-li' href='/' > PassportsHub </a></li>
+                        <li ><a className='navbar-li' href='/about'> About Me</a></li>
                         {/* <li ><a className='navbar-li' href='/about' style={{'marginLeft':'100px'}}> FaceBook </a></li> */}
-                        <li ><a className='navbar-li' href='https://www.instagram.com/mokhtar.egypt/' target='_blank'> Instagram </a></li>
+                        {/* <li ><a className='navbar-li' href='https://www.instagram.com/passportshub/' target='_blank'> Instagram </a></li> */}
                         
                     </ul>
-                </div>
-                <div className='space-2'></div>
+                {/* </div> */}
+                {/* <div className='space-2'></div> */}
             </nav>
         </header>
     );
