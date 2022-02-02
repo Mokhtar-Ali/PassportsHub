ec68f185758ee156039380eaf6ae84081e992e54
Mokhtar Ali <Joemokhtarali@gmail.com>
2022-02-01 19:33:18 -0500

    eslint setup

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 import NavBar from './components/Navbar';
 import About from './components/About';
-import Egypt from './pages/Egypt';
-import Thailand from './pages/Thailand';
-import Ecuador from './pages/Ecuador';
-import Colombia from './pages/Colombia';
 
 
 
@@ -18,10 +14,12 @@ ReactDOM.render(
     <NavBar />
     <Route exact path="/" component={App} />
     <Route exact path="/about" component={About} />
-    <Route exact path="/egypt" component={Egypt} />
+    <Route exact path="/cities/:id" component={About} />
+
+    {/* <Route exact path="/egypt" component={Egypt} />
     <Route exact path="/thailand" component={Thailand} />
     <Route exact path="/ecuador" component={Ecuador} />
-    <Route exact path="/colombia" component={Colombia} />
+    <Route exact path="/colombia" component={Colombia} /> */}
 
   </Router>
   , document.getElementById('root'))
